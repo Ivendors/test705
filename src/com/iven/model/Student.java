7eba83d3127bb43247462067454c762bf855a4bd
zs <zs@bjpowernode.com>
2020-07-05 18:35:06 +0800

    创建Student

---
+++ src/com/iven/model/Student.java
@@ -0,0 +1,14 @@
+package com.iven.model;
+
+/**
+ * ClassName:Student
+ * Package:com.iven.model
+ * Description:
+ *
+ * @date:2020/7/5 下午 6:30
+ * @author:Ivendors
+ */
+public class Student {
+    private String name;
+    private Integer age;
+}
